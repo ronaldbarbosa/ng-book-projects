ac53104e9ca79a51e50a37694419dffde86c3169
Ronald Barbosa <ronaldverissimo@hotmail.com>
2022-08-21 00:35:18 -0300

    Implementação AngularReddit

---
+++ 01-writing-first-angular-webapp/angular-reddit/src/app/article/article.model.ts
@@ -0,0 +1,28 @@
+export class Article {
+    title: string;
+    link: string;
+    votes: number;
+
+    constructor(title: string, link: string, votes?: number) {
+        this.title = title;
+        this.link = link;
+        this.votes = votes || 0;
+    }
+
+    voteUp(): void {
+        this.votes++;
+    }
+    
+    voteDown(): void {
+        this.votes--;
+    }
+    
+    domain(): string | null {
+        try {
+            const domainAndPath: string = this.link.split('//')[1];
+            return domainAndPath.split('/')[0];
+        } catch (error) {
+            return null;
+        }
+    }
+}
